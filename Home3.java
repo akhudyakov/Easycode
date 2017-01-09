3a3a22f23b126ebca3d36f195d139a392249feb9
Khudyakov Andrei <baltica.andrey@gmail.com>
2017-01-09 20:34:47 +0300

    Add files via upload
    up to 2 sized arrays

--- Home3.java
+++ Home3.java
@@ -1,6 +1,8 @@
 package HomeTask_3;//package com.company;
 
 
+import com.sun.deploy.util.ArrayUtil;
+
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -51,7 +53,7 @@ public class Home3 {
 
         //  Origin Array
 
-        //int originArray[] = {-9,  26, -3, 5, 12, -30,  17, -20, 6 }; // 12 items
+        //int originArray[] = {-9,  26, -3, 5, 12, -30,  17, -20, 6 }; // 9 items without 0
         int originArray[] = {-9, 0, 26, -3, 5, 12, -30, 0, 17, -20, 6, 0 }; // 12 items
 
         // Calculate negative items of the origin array
@@ -137,16 +139,44 @@ public class Home3 {
 
         // Calculte sum of Math.abs of array's items after First 0
 
-        int sumModulesOfItemAfterFirstZero = calculateSumModulesOfItemAfterFirstZero (originArray);
-        System.out.println("\n **** Sum of items' modules after first 0 = " + sumModulesOfItemAfterFirstZero);
+        int findValue = 0;
+
+        int firstPositionValue = findFirstPositionOfItemInArray(originArray, findValue);
+        int sumModulesOfItemAfterFirstZero = calculateSumModulesOfItems(originArray, firstPositionValue + 1);
+
+        if (sumModulesOfItemAfterFirstZero > 0) {
+            System.out.println("\n **** Sum of items' modules after first " + findValue + " = " + sumModulesOfItemAfterFirstZero + ". Expected: 119");
+        } else {
+            System.out.println("\n There is no value " + findValue + " in the array to calculate sum of items' modules after " + findValue);
+        }
+
+        // Create an array from origin array next way:
+        // even members are in the start of an array
+        // odd members are in the end of the array
+
+        int [] newEvenOddArray = Arrays.copyOf(originArray, originArray.length); // do not use origin array
+
+        newEvenOddArray = getArrayEvenMemeberFirst(newEvenOddArray);
+        System.out.println(" New array with First members are Even: " + Arrays.toString(newEvenOddArray) + ". Expected {-9, 26, 5, -30, 17, 6, 0, -3, 12, 0, -20, 0 } ");
 
 
+        // Two sized array
 
+        int originTwoArray [] [] =              { {10, 0, -6, 33, 33, -60, 4},
+                                                   {-19, 73, 73, 73, 0, -55, 1},
+                                                   {2, 7, -42, 10, 18, 8, -12 },
+                                                   {0, 13, -22, 11, 88, 15, -1},
+                                                   {9, 60, 4, -5, 6, 63, 28},
+                                                   {3, 5, 5, 5, 5, 18, -1},
+                                                   {8, - 13, 44, -2, 9, 14, 17}   };
 
-
+        int max = maxItemOfArray(originTwoArray) ;
+        System.out.println(max);
 
 
     }
+
+
 
 
 
@@ -199,7 +229,7 @@ public class Home3 {
 
         int multipleItems = 1; // != 0 because it is start for multiplication
 
-        if (positionMin > positionMax) { //we need to compare postions of Max and Min items for creating cycyle FOR
+        if (positionMin > positionMax) { //we need to compare postions of Max and Min items for creating cycle FOR
 
             for (int i = positionMax + 1; i < positionMin; i++) { //don't include postions of Min and Max
                 multipleItems = multipleItems * arr[i];
@@ -216,7 +246,7 @@ public class Home3 {
 
     public static void findFirstAndLastPositionOfZero (int arr[]) {
 
-        int firstPositionOfZero = -1; // it is possible that there is n zero in array
+        int firstPositionOfZero = -1; // it is possible that there is no zero in array
         int lastPositionOfZero = -1;
         boolean existZero = false;
 
@@ -334,8 +364,114 @@ public class Home3 {
     }
 
 
-    public static int calculateSumModulesOfItemAfterFirstZero (int [] arr) {
+    public static int findFirstPositionOfItemInArray (int [] arr, int value) {
 
+        int positionFirstValue = -100; // there is no the Value in the array
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] == value) {
+                positionFirstValue = i;
+                return positionFirstValue; // find first encounter of the Value
+            }
+
+        }
+        return positionFirstValue;
+    }
+
+    public static int calculateSumModulesOfItems (int [] arr, int startPositionValue) {
+
+        int sumModules = 0;
+
+        if (startPositionValue >= 0) {
+            for (int i = startPositionValue; i < arr.length; i++) {
+                sumModules = sumModules + Math.abs(arr[i]);
+            }
+        } else {
+            sumModules = -1;  // there is no the Value in the array
+        }
+        return sumModules;
+    }
+
+
+
+    public static int [] getArrayEvenMemeberFirst (int [] arr) {
+
+        //int lengthArr = arr.length;
+        //boolean evenMembers = false;
+
+        int lenghtOddArr =  arr.length/2;
+        int lenghtEvenArr =  arr.length/2;
+
+
+        if (arr.length%2 != 0 ) {
+        // evenMembers = true;
+        lenghtEvenArr = lenghtEvenArr + 1;
+        }
+
+
+        int [] evenArr = new int [lenghtEvenArr];
+        int [] oddArr = new int [lenghtOddArr];
+
+        int evenCounter = 0;
+        int oddCounter = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (i%2 == 0) {
+                evenArr[evenCounter] = arr[i];
+                evenCounter ++;
+            } else {
+             oddArr[oddCounter] = arr[i];
+             oddCounter ++;
+            }
+        }
+
+        for (int i = 0; i < evenCounter; i++) {
+            arr [i] = evenArr[i];
+        }
+
+        for (int i = 0; i < oddCounter ; i++) {
+            arr[evenCounter] = oddArr[i];
+            evenCounter ++;
+        }
+        //arr = Arrays.copyOfRange(oddArr, 0, oddCounter);
+        return arr;
+    }
+
+
+    public static int maxItemOfArray(int [] [] arr) {
+
+        int maxItem = arr [0] [0];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (maxItem < arr[i] [j]) {
+                    maxItem = arr[i] [j];
+                }
+
+            }
+
+        }
+        return maxItem;
+    }
+
+
+    // get number of rows without some value
+
+
+
+    public static int getNumberOfRowsWithoutSomeValue(int [] [] arr, int value) {
+
+        StringBuffer  existValueInRow = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                if (arr[i] [j] == value) {
+                    if ((existValueInRow.indexOf(String.valueOf(i)))) {
+
+                 existValueInRow.append(i);
+                }
+        }
     }
 }
 
