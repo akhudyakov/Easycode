c7a2bf427eeeba328f1236ca3131282c72a1b4de
Khudyakov Andrei <akhudyakov@metratech.com>
2017-01-16 02:44:42 +0200

    Add Group.class and add newStudent to newGroup

--- MyInstitute.java
+++ MyInstitute.java
@@ -12,11 +12,15 @@ public class MyInstitute {
         public static void main(String[] args) {
 
 
-//            Student newStudent = new Student ("Ivanov", "Petr", "Sergeevich", Date(1973,11,25), "passport", "MK123456" );
-            Student newStudent = new Student ("Ivanov", "Petr", "Sergeevich", Date.valueOf("1973-11-25"), "passport", "MK123456" );
-            String d = newStudent.toString();
+//            Student newStudent1 = new Student ("Ivanov", "Petr", "Sergeevich", Date(1973,11,25), "passport", "MK123456" );
+            Student newStudent1 = new Student ("Ivanov", "Petr", "Sergeevich", Date.valueOf("1973-11-25"), "passport", "MK123456" );
+            String d = newStudent1.toString();
             System.out.println(d);
 
+            Group newGroup = new Group(1, 3 , "7");
+            newGroup.addStudentToGroup(newGroup.getIdGroup(), newStudent1.getId());
+            System.out.println(newGroup.toString());
+            System.out.println((newGroup.getCountOfGroupMembers(newGroup.getIdGroup())));
 
         }
 }
